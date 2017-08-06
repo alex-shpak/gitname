11f2a2dc989b3f4c33ac814d11b2010eed86b1ae
Alex Shpak <alex-shpak@users.noreply.github.com>
2017-08-06 11:54:58 +0200

    Added some input validation

--- gitname.py
+++ gitname.py
@@ -8,12 +8,23 @@ from os.path import expanduser
 
 
 status, remote = commands.getstatusoutput('git remote get-url origin')
+if status is not 0:
+    print 'No remote found in'
+    os.system('git config --list')
+    quit()
 
-hostname = urlparse('scheme://%s' % remote).hostname  # not 100% correct
+if '://' not in remote:
+    remote = 'scheme://%s' % remote
+
+hostname = urlparse(remote).hostname
 
 config_defaults = { 'name': None, 'email': None }
 config = ConfigParser.SafeConfigParser(defaults = config_defaults)
 config.read('%s/.gitname' % expanduser('~'))
+
+if not config.has_section(hostname):
+    print 'No section "%s" found in ~/.gitname' % hostname
+    quit()
 
 git_name = config.get(hostname, 'name')
 git_email = config.get(hostname, 'email')
